0a4970d9e39a74e1a00758c09a90a36aba76ec83
John Hansen <jhb.rgn@gmail.com>
2024-01-10 15:10:56 -0500

    Create superuser

--- main_app/templatetags/custom_filters.py
+++ main_app/templatetags/custom_filters.py
@@ -2,9 +2,7 @@ from django import template
 
 register = template.Library()
 
+# Returns the value turned into a list.
 @register.filter(name='split')
 def split(value, key):
-    """
-    Returns the value turned into a list.
-    """
     return value.split(key)
