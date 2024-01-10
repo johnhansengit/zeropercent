a5aa9e70bc053caafc1647c20b13e4e5b08ff8bd
John Hansen <jhb.rgn@gmail.com>
2024-01-10 13:17:46 -0500

    Add model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    ingredients = models.TextField(max_length=500)
+    instructions = models.TextField(max_length=1000, blank=True)
+    difficulty = models.IntegerField()
+    prep = models.IntegerField(null=True, blank=True)
+    img = models.URLField(blank=True)
