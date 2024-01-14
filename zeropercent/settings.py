c013d1be5bb3247ce57a6adb66da7dbe650e2a5f
John Hansen <jhb.rgn@gmail.com>
2024-01-14 10:11:05 -0500

    Update database config for Heroku

--- zeropercent/settings.py
+++ zeropercent/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,11 +78,9 @@ WSGI_APPLICATION = 'zeropercent.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'zeropercent',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
 
 
 # Password validation
